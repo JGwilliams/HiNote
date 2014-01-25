250e093dc1b4578df4b7ef7555bf712469c1979a
Jonathan Gwilliams <eldritchdesign@aol.com>
2014-01-25 08:58:39 +0000

    Initial Commit

---
+++ HiNote/SSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  SSAppDelegate.h
+//  HiNote
+//
+//  Created by Jonathan Gwilliams on 25/01/2014.
+//  Copyright (c) 2014 Sane Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ HiNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HiNote
+//
+//  Created by Jonathan Gwilliams on 25/01/2014.
+//  Copyright (c) 2014 Sane Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
